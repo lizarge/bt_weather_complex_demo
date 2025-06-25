b11e182e7e568798eeb759ca0df9309cfb71692e
ankudinov aleksander <sasha@ankudinov.org.ua>
2025-06-25 12:17:56 +0300

    Initial Commit

---
+++ droid/droidApp.swift
@@ -0,0 +1,17 @@
+//
+//  droidApp.swift
+//  droid
+//
+//  Created by ankudinov aleksandr on 25.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct droidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
